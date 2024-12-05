d3aefe58346e0b8f04ea870a7dfe9b9093fa1e44
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-12-05 20:32:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,9 @@ let wsw;
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-function setPixel(x, y, r, g, b) {
-  const imageData = ctx.createImageData(1, 1);
-  const data = imageData.data;
-
-  data[0] = r;
-  data[1] = g;
-  data[2] = b;
-  data[3] = 255;
-
-  // Draw the pixel on the canvas
-  ctx.putImageData(imageData, x, y);
+function drawPixel(x, y, r, g, b) {
+  ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+  ctx.fillRect(x, y, 1, 1);
 }
 
 let inp = document.querySelector('input');
